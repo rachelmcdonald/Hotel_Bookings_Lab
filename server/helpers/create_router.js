0f1bb6cfe96b193f418a0ff46dfe3c9cf6c6f0f5
Rachel McDonald <rach-mcdonald@outlook.com>
2022-03-09 13:26:46 +0000

    CRUD functionality written and is working

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -4,11 +4,42 @@ const ObjectID = require('mongodb').ObjectID;
 const createRouter = function (collection) {
     const router = express.Router();
 
+    //INDEX
     router.get('/', (req, res)=>{
         collection
             .find()
             .toArray()
             .then((docs)=>res.json(docs))
+            .catch((err)=>{
+                console.error(err);
+                res.status(500);
+                res.json({status: 500, error: err});
+            });
+    });
+
+    //CREATE
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection
+            .insertOne(newData)
+            .then((result) => {
+                res.json(result.ops[0])
+            })
+            .catch((err)=>{
+                console.error(err);
+                res.status(500);
+                res.json({status: 500, error: err});
+            });
+    });
+
+    //DELETE
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id 
+        collection 
+            .deleteOne({_id: ObjectID(id)})
+            .then((result) => {
+                res.json(result)
+            })
             .catch((err)=>{
                 console.error(err);
                 res.status(500);
