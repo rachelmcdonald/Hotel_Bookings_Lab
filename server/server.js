731ae651269890b70716f7317b0be937b5416c8f
Steph Paterson <steph.c.paterson@gmail.com>
2022-03-09 12:07:22 +0000

    first route tested and working

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
 
 app.use(cors());
 app.use(express.json());
+
+MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
+    .then((client)=>{
+        const db = client.db('hotel_bookings');
+        const bookingsCollection = db.collection('bookings');
+        const bookingsRouter = createRouter(bookingsCollection);
+        app.use('/api/bookings', bookingsRouter);
+    })
+    .catch(console.error);
+
+app.listen(5000, function (){
+    console.log(`listening on port ${this.address().port}`);
+});
