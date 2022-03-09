f9d0e969eb230172ce6deb21a26f3879c6b4da4a
Rachel McDonald <rach-mcdonald@outlook.com>
2022-03-09 14:29:45 +0000

    Customer Data now in the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Form from './Components/Form';
 import BookingsContainer from './Containers/BookingsContainer';
+import { getBookings } from './BookingsService';
 
 function App() {
+
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    getBookings()
+    .then((data) => {
+      setBookings(data)
+    })
+  }, [])
+
   return (
     <>
     <h1>Hotel still to be named..</h1>
